30f70f4ab90fd73b0b7933adc2d027dc13d93e19
noahFavelo <nnfavelo@gmail.com>
2024-11-08 20:21:59 -0500

    set up workspace

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.use('/', (req, res, next) => {
+  res.status(404).send(' you got a generic 404, thats an error!');
+});
+
+app.listen(3000); //listens on port 3000 -> http://localhost:3000/
